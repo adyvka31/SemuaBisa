45ef56b78d2db359f589cf38d61043b63a8a8d9b
mirzadfrpl <mirzadf123@gmail.com>
2024-09-26 21:05:59 +0700

    new

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-// js untuk menghilangkan popup atas
+
 document.getElementById('close-btn').addEventListener('click', function() {
     document.getElementById('sticky-banner').classList.add('hidden');
 });
+
+// -------
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('nav');
+    if (window.scrollY > 0) {
+        navbar.classList.add('shadow-md', 'border-b'); // Menambahkan bayangan dan border saat di-scroll
+    } else {
+        navbar.classList.remove('shadow-md', 'border-b'); // Menghapus bayangan dan border saat di atas
+    }
+});
+
